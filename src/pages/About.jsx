6523454f7144058622bb22e1fc6f0fb83b6d3706
--get <debadarshanmaharana248@gmail.com>
2025-01-01 00:35:09 +0530

    Little modify on About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/Navbar";
 import React, { useState } from "react";
-import giottoFood from "../assets/GiottoFoodUK.png";
+import giottoFood from "../assets/giottoFoodUk.png";
 import olio from "../assets/Olio Dante.mp4";
 import Footer from "@/components/Footer";
 import fieldTree from "../assets/Field_Tree.jpg";
